bd24f7076db6a4c09ef745867b1bb1ad25e047e6
PRINZOP <prinz.lim@yahoo.com>
2026-03-23 16:35:12 +0800

    function time
    yes

--- CCDSTRU.c
+++ CCDSTRU.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
+void Remove(int pos){ // parameters not final, can change
+	
+}
+
+void Replace(int pos){
+	
+}
+
+void Expand(int pos){
+	
+}
+
+void Update(int pos){
+	
+}
+
+void NextPlayerMove(){
+	
+}
+
+void GameOver(){
+	
+}
+
 int main(){
-	printf("hey guys its me prinzop I love ccdstru :) no jokes here! haha");
-    printf("lmnfaodadsf");
-    printf("lmfao");
+	int good,go,start,over,found,val; // boolean values & val, which is a positive integer including 0
+	int 
+	// i dunno how to initialize the arrays pls help
+	// System Initialization
+	good = 0;
+	go = 1;
+	start = 1;
+	found = 0;
+	val = 0;
 }
