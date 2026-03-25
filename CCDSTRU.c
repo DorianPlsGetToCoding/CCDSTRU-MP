c3c38f816a87ce7b273d3b792076773b1734f585
PRINZOP <prinz.lim@yahoo.com>
2026-03-25 20:59:40 +0800

    New helper function: add()
    add():
    - Adds key to an array and increments
    the number of elements by 1

--- CCDSTRU.c
+++ CCDSTRU.c
@@ -1,77 +1,51 @@
 #include <stdio.h>
+#include "helperFunctions.h"
 
-void Expand(int x,int y,int grid[][3],int go,int R[],int B[],int S[],int T[]){
-	// not sure how to add an array to an array, I'll stop here
+void
+NextPlayerMove(){
+	
 }
 
-void Update(int x,int y,int grid[][3],int go,int *good,int R[],int B[],int S[],int T[]){
-	*good = 0;
-	if(pos not in S){ // change
-		// add pos to S
-		*good = !*good;
-	}
-	if(*good&&pos in S&&pos not in T){ // change
-		// add pos to T
-		Expand(x,y,grid,go,R,B,S,T);
-	}
-}
-
-void NextPlayerMove(int grid[][3],int x,int y,int *go,int *start,int *over,int *val,int *good,int R[],int B[],int S[],int T[]){
-	if(!*over){ // if game is not over
-		if(*start){ // if start is true
-			if(*go){ // player R turn
-				// add grid[x][y] to R
-				// add grid[x][y] to S
-				good = 1;
-			}else{ // player B turn
-				// add grid[x][y] to B
-				// add grid[x][y] to S
-				good = 1;
-			}
-			if((*go&&pos in R)||(!*go&&pos in B){ // replace this
-				int tempGo = *go;
-				int tempGood = *good;
-				Update(x,y,grid,tempGo,&tempGood,R,B,S,T);
-				*good = tempGood;
-			}
-		}
-	}
-	if(*start&&#elements in R is 1&&#elements in B is 1){ // this too
-		*start = 0;
-	}
-	if(!*over&&good){
-		*good = !*good;
-		*go = !*go;
-		*val = *val+1;
-	}
-}
-
-/*
-	TO-DO:
-	try creating a manual check that makes the grid act like a strict grid
-	e.g. grid[0][3] should theoretically be out of bounds, but it goes to the next row and returns 4
-*/
-int main(){
-	int good,go,start,over,found,val;
+int
+main(){
 	int grid[3][3] = { // this is the playing field that will be displayed for reference, this does not change
 		{1,2,3},
 		{4,5,6},
 		{7,8,9},
 	};
-	int R[9]; // this represents the tiles claimed by R
-	int B[9]; // this represents the tiles claimed by R
-	int S[9]; // this represents occupied tiles
-	int T[9]; // if a tile is placed here, it will undergo splitting into 3 directions
+	/*
+		NOTES:
+		1. the found variable (boolean) is only used in the Replace function, no need to define it here
+		2. the game ends if and only if:
+			- Only 3 free tiles remain on the board
+			- 20 or more turns have passed
+			- if start is false and the board ONLY contains Player R/Player B tiles
+	*/
+	int good = 0; // boolean value, used in updating the board and game state
+	int go = 1; // boolean value, determines player turn (Player R if true, and Player B if false)
+	int start = 1; // boolean value, determines if the game has started and keeps it going
+	int val = 0; // determines how many turns have passed with a maximum of 20 turns before the game automatically ends
 	
-	// initializing values
-	good = 0;
-	go = 1;
-	start = 1;
-	found = 0;
-	val = 0;
-	over = 0;
-	printf("%d\n",R[0]);
-	NextPlayerMove(grid,0,0,&go,&start,&over,&val,&good,R,B,S,T);
-	printf("%d\n",R[0]);
+	int R[9]; // an array that represents the tiles claimed by Player R
+	int rSize = 0; // the current number of elements in R[]
+	int B[9]; // an array that represents the tiles claimed by Player B
+	int bSize = 0; // the current number of elements in B[]
+	int S[9]; // an array that represents currently occupied tiles by all players
+	int sSize = 0; // the current number of elements in S[]
+	int T[9]; // an array that is responsible for handling cascading tilespread
+	int tSize = 0; // the current number of elements in T[]
 	
+	int test = 1;
+	int x=0,y=0;
+	while(test){
+		printf("x: ");
+		do{
+			scanf("%d",&x);
+		}while(x<0||x>2);
+		printf("y: ");
+		do{
+			scanf("%d",&y);
+		}while(y<0||y>2);
+		NextPlayerMove
+	}
 }
